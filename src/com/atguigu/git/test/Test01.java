cd1db07989ec92ae3501b8c7c92839267f840e68
xm1075089384 <1075089384@qq.com>
2016-01-06 00:59:23 +0800

    git update03
    git update03

--- src/com/atguigu/git/test/Test01.java
+++ src/com/atguigu/git/test/Test01.java
@@ -5,8 +5,9 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello git 01");
 		System.out.println("hello git 02");
-		System.out.println("giyhub update 02");
-		System.out.println("local update 02");
+		System.out.println("github update 02");
+		System.out.println("local update 01");
+		System.out.println("github update 03");
 	}
 	
 }
